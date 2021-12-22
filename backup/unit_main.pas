d62d72093d42a8724bead14c9baf233b9eba687d
Ivan <49370268+ivekarp@users.noreply.github.com>
2021-12-22 18:08:38 +0500

    Add files via upload

---
+++ backup/unit_main.pas
@@ -0,0 +1,69 @@
+unit unit_main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, strutils, Clipbrd;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Label1: TLabel;
+    Label2: TLabel;
+    input_text: TMemo;
+    output_text: TMemo;
+    procedure Button3Click(Sender: TObject);
+    procedure transform_text();
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.transform_text();
+begin
+  output_text.Clear;
+  output_text.Lines.Text :=
+  StringsReplace(input_text.Lines.Text,
+                 ['а', 'е', 'о', 'у', 'х', 'я', 'з', 'и', 'п', 'с', 'ь', 'Х'],
+                 ['a', 'e', 'o', 'y', 'x', '9', '3', 'u', 'n', 'c', 'b', 'X'],
+                 [rfReplaceAll]);
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  Clipboard.AsText := output_text.Text;
+  ShowMessage('Текст скопирован в буфер обмена!');
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  transform_text();
+  ShowMessage('Готово!');
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  input_text.Clear;
+end;
+
+end.
+
